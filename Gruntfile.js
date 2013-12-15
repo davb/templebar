82fd8618c9fe1edd3b986e8067fe41349aaa29df
Davide <davide@feeligo.com>
2013-12-15 15:56:12 +0100

    version 0.1.0; added Grunt toolchain

---
+++ Gruntfile.js
@@ -0,0 +1,33 @@
+module.exports = function(grunt) {
+
+  grunt.loadNpmTasks('grunt-contrib-coffee');
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    clean: {
+      js: ['<%= pkg.name %>*.js'],
+      tmp: ['.tmp']
+    },
+    coffee: {
+      dist: {
+        files: {
+          '.tmp/<%= pkg.name %>-<%= pkg.version %>.js': 'src/*.coffee'
+        }
+      }
+    },
+    uglify: {
+      options: {
+        banner: '/*! <%= pkg.name %> <%= grunt.template.today("dd-mm-yyyy") %> */\n'
+      },
+      dist: {
+        files: {
+          '<%= pkg.name %>-<%= pkg.version %>.min.js': ['.tmp/<%= pkg.name %>*.js']
+        }
+      }
+    }
+  });
+
+  grunt.registerTask('default', ['clean', 'coffee', 'uglify', 'clean:tmp']);
+};
